56af60d4bd39b02809767b93397ad0602782f825
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-17 18:29:34 -0300

    feat(MainScreen): add DropdownMenu for options

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/intentscompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/intentscompose/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -25,10 +27,26 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    var expanded by remeber { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Intents Example") }
+                title = { Text("Intents Example") },
+                actions = {
+                    IconButton(onClick = { expanded = !expanded}) {
+                        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Menu")
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false}
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("SetParameter")},
+                            OnClick = { expanded = false}
+                        )
+                    }
+                }
             )
         },
         content = { padding ->
