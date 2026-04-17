9893e795de8564e975e4ad35220583340b6b20fd
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-17 18:35:25 -0300

    feat(MainScreen): add call and dial functionality

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/intentscompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/intentscompose/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.intentscompose
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +12,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import br.edu.ifsp.scl.bes.prdm.sc304453x.intentscompose.ui.theme.IntentsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +30,17 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    var expanded by remeber { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    val callOrDialPhoneNumber = { call: Boolean ->
+        val intent = if (call) {
+            Intent(Intent.ACTION_CALL, Uri.parse("tel:123456789"))
+        } else {
+            Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789"))
+        }
+        context.startActivity(intent)
+    }
 
     Scaffold(
         topBar = {
@@ -44,6 +57,20 @@ fun MainScreen() {
                         DropdownMenuItem(
                             text = { Text("SetParameter")},
                             OnClick = { expanded = false}
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Make Call") },
+                            onClick = {
+                                expanded = false
+                                callOrDialPhoneNumber(true)
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Dial Number") },
+                            onClick = {
+                                expanded = false
+                                callOrDialPhoneNumber(false)
+                            }
                         )
                     }
                 }
